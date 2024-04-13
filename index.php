225f4b99403ce43f46a81ec6d2b51ee76c9368fe
Lizeth-SP <119645514+Lizeth-SP@users.noreply.github.com>
2024-04-12 20:58:21 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
         <title align = "center">Pagina Laboratorio 3.1</title>
     </head>
     <body>
-        <?php echo '<p>Hola Mundo</p>'?>
+        <?php echo '<p>Hola a todos y todas, que tengan un excelente día</p>'?>
     </body>
 </html>
